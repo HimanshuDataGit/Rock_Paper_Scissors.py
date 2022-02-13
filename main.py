f7f791705ef03099e093c039fdc98e22e353da28
Himanshu Tiwari <98234068+HimanshuDataGit@users.noreply.github.com>
2022-02-13 14:10:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import random
-<<<<<<< HEAD
-choice =  int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n'))
 
-=======
-choice =  int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. '))
-user = 0
-pc = 0
->>>>>>> 61fa1d98fc38261fd2c8ada85c7be49481f7bc4c
+choice =  int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n'))
 computer = random.randint(0,2)
 user = choice
 pc = computer
@@ -67,12 +61,9 @@ elif computer == 2:
 if (choice >2 or choice<0):
     print("You choose an invalid number, please choose a valid number.")     
 elif (choice == computer):
-        print(f"Player:{user}\nComputer:{pc}\n\nIt's a Draw!!.")
+    print(f"Player:{user}\nComputer:{pc}\n\nIt's a Draw!!.")
 elif(choice == 0 and computer == 2) or (choice == 1 and computer == 0) or (choice ==2 and computer==1):
-        print(f'Player:{user}\nComputer:{pc}\n\nCongrats!! You won.')
+    print(f'Player:{user}\nComputer:{pc}\n\nCongrats!! You won.')
 elif(computer==0 and choice==2) or (computer==1 and choice==0) or (computer==2, choice==1):
-<<<<<<< HEAD
-        print(f'Player:{user}\nComputer:{pc}\n\nBad luck!! you lost.')
-=======
     print(f'Player:{user}\nComputer:{pc}\n\nBad luck!! you lost.')
->>>>>>> 61fa1d98fc38261fd2c8ada85c7be49481f7bc4c
+
