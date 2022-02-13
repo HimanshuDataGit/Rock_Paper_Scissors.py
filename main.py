dc37b6ccc4ea154bda53fb3389ad13f36a386539
HimanshuDataGit <himanshutiwariiiter@gmail.com>
2022-02-13 14:04:02 -0600

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
-choice =  int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. '))
-user = "0"
-pc = "0"
+choice =  int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n'))
+
 computer = random.randint(0,2)
+user = choice
+pc = computer
+
 
 if choice ==1:
     user = '''
@@ -56,10 +58,11 @@ elif computer == 2:
       ---.__(___)'''
 
 
-
-if (choice == computer):
-    print(f"Player:{user}\nComputer:{pc}\n\nIt's a Draw!!.")
+if (choice >2 or choice<0):
+    print("You choose an invalid number, please choose a valid number.")     
+elif (choice == computer):
+        print(f"Player:{user}\nComputer:{pc}\n\nIt's a Draw!!.")
 elif(choice == 0 and computer == 2) or (choice == 1 and computer == 0) or (choice ==2 and computer==1):
-    print(f'Player:{user}\nComputer:{pc}\n\nCongrats!! You won.')
+        print(f'Player:{user}\nComputer:{pc}\n\nCongrats!! You won.')
 elif(computer==0 and choice==2) or (computer==1 and choice==0) or (computer==2, choice==1):
-    print(f'Player:{user}\nComputer:{pc}\n\nBad luck!! you lost.')
+        print(f'Player:{user}\nComputer:{pc}\n\nBad luck!! you lost.')
